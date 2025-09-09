07a373e91bd989bbeabaae506ee2a73bea4af7ac
Pongstorm Potita <gaymanxii1995@gmail.com>
2025-09-09 12:07:06 +0700

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+}
